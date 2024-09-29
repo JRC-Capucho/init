1a3da91c3776afb75b0b8630c8c599f2db1fc41a
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-09-29 19:29:56 -0300

    change theme for catppuccin

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,15 +1,27 @@
 return {
-  'rose-pine/neovim',
-  name = 'rose-pine',
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
   config = function()
-    require('rose-pine').setup {
-      styles = {
-        transparency = true,
-      },
+    require('catppuccin').setup {
+      transparent_background = true,
     }
-    vim.cmd.colorscheme 'rose-pine-moon'
-    -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-    -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-    -- vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+    vim.cmd.colorscheme 'catppuccin'
   end,
 }
+
+-- {
+--   'rose-pine/neovim',
+--   name = 'rose-pine',
+--   config = function()
+--     require('rose-pine').setup {
+--       styles = {
+--         transparency = true,
+--       },
+--     }
+--     vim.cmd.colorscheme 'rose-pine-moon'
+--     -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+--     -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+--     -- vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+--   end,
+-- }
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,12 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  {
+    'barrett-ruth/live-server.nvim',
+    build = 'npm i -g live-server',
+    cmd = { 'LiveServerStart', 'LiveServerStop' },
+    config = true,
+  },
 
   {
     'kristijanhusak/vim-dadbod-ui',
