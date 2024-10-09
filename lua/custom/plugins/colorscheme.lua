d846363fd5872d323fe95d133414fd95e7b23b3e
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2024-10-09 19:35:09 -0300

    change theme for neofusion

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,27 +1,8 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
+  'diegoulloao/neofusion.nvim',
   priority = 1000,
   config = function()
-    require('catppuccin').setup {
-      transparent_background = true,
-    }
-    vim.cmd.colorscheme 'catppuccin'
+    vim.o.background = 'dark'
+    vim.cmd.colorscheme 'neofusion'
   end,
 }
-
--- {
---   'rose-pine/neovim',
---   name = 'rose-pine',
---   config = function()
---     require('rose-pine').setup {
---       styles = {
---         transparency = true,
---       },
---     }
---     vim.cmd.colorscheme 'rose-pine-moon'
---     -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
---     -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
---     -- vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
---   end,
--- }
