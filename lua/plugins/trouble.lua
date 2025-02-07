07fd32591234ead5129276a35a2b35eb43e51b9e
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2025-02-06 22:03:44 -0300

    adding trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,16 @@
+return {
+  'folke/trouble.nvim',
+  config = function()
+    vim.keymap.set('n', '<leader>tt', function()
+      require('trouble').toggle 'diagnostics'
+    end)
+
+    vim.keymap.set('n', '[t', function()
+      require('trouble').next { skip_groups = true, jump = true }
+    end)
+
+    vim.keymap.set('n', ']t', function()
+      require('trouble').prev { skip_groups = true, jump = true }
+    end)
+  end,
+}
