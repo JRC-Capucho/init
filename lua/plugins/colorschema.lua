0892a79da024c09bd26679a1f92db2a42621889b
JRC-Capucho <joaorobertoc.capucho@gmail.com>
2025-04-14 22:47:28 -0300

    feat(ui): switch to vesper colorscheme and add auto-tags
    Replace rose-pine with vesper colorscheme for a new visual experience,
    while keeping other theme options commented for future reference.
    
    Add nvim-ts-autotag plugin to automatically close and rename HTML/JSX
    tags when working with JavaScript and TypeScript files.

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,8 +1,32 @@
--- lua/plugins/rose-pine.lua
 return {
-  'rose-pine/neovim',
-  name = 'rose-pine',
+  'datsfilipe/vesper.nvim',
+  priory = 1000,
   config = function()
-    vim.cmd 'colorscheme rose-pine'
+    vim.cmd 'colorscheme vesper'
   end,
 }
+
+-- return {
+--   'sainnhe/sonokai',
+--   priory = 1000,
+--   config = function()
+--     vim.cmd 'colorscheme sonokai'
+--   end,
+-- }
+
+-- return {
+--   'navarasu/onedark.nvim',
+--   opts = {},
+--   config = function()
+--     vim.cmd 'colorscheme onedark'
+--   end,
+-- }
+--
+-- lua/plugins/rose-pine.lua
+-- return {
+--   'rose-pine/neovim',
+--   name = 'rose-pine',
+--   config = function()
+--     vim.cmd 'colorscheme rose-pine'
+--   end,
+-- }
